95b0b98c176c79b230dc4afc69a012630fe30c7a
Sean DePotter <seandep@live.com>
2019-06-19 02:34:26 -0400

    Add project files.

---
+++ Classes Exercise 2/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Classes_Exercise_2
+{
+    public class Post
+    {
+        private string _title;
+        private string _description;
+        private string _date;
+        private string _time;
+        public int VoteValue { get; private set; } = 0;
+
+        public Post()
+        {
+            Console.Write("Give me a Title: ");
+            _title = Console.ReadLine();
+            Console.Write("Give me a description: ");
+            _description = Console.ReadLine();
+            _date = DateTime.Now.ToShortDateString();
+            _time = DateTime.Now.ToShortTimeString();
+
+            Console.WriteLine(@"Post: {0}
+Description: {1}
+Date: {2}
+Time: {3}",_title, _description , _date , _time);
+        }
+
+        public void UpVote()
+        {
+            VoteValue++;
+        }
+
+        public void DownVote()
+        {
+            VoteValue--;
+        }
+
+    }
+}
---
+++ Classes Exercise 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+
+
+            Console.WriteLine(post.VoteValue);
+        }
+    }
+}
